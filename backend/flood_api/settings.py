a6a7422dea228b9c6175dd243a82560e66fc1df6
FuzelahamedShaik <fuzelahamed1999@gmail.com>
2026-06-06 02:23:42 +0300

    Temporarily allow all CORS for demo

--- backend/flood_api/settings.py
+++ backend/flood_api/settings.py
@@ -77,15 +77,16 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
 
-CORS_ALLOWED_ORIGINS = [
-    "https://fuzelahamedshaik.github.io",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://fuzelahamedshaik.github.io",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://fuzelahamedshaik.github.io",
+# ]
+#
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://fuzelahamedshaik.github.io",
+# ]
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
